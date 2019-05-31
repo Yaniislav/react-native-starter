d2603d843ca5b45ff51a7f95865ac67dc5222917
Anatolii <anatolii@bambu.life>
2019-05-31 08:25:14 +0300

    update redux-saga added changes to mainSaga

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,8 +1,8 @@
-import { fork } from 'redux-saga/effects';
+import { all } from 'redux-saga/effects';
 import authSaga from './auth-saga';
 
 export default function* rootSaga() {
-  yield [
-    fork(authSaga),
-  ];
+  yield all([
+    authSaga(),
+  ]);
 }
