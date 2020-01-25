eab738371a6e81581726c576053d0f9211c78bb7
mikhail <mikhail.shvets96@gmail.com>
2020-01-25 16:19:01 +0200

    updated RN version

--- ios/Starter/AppDelegate.m
+++ ios/Starter/AppDelegate.m
@@ -20,7 +20,6 @@
                                                    moduleName:@"Starter"
                                             initialProperties:nil];
 
-
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
